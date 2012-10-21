b7c3741c21aeb500ff5609406cc850e12e6c051e
Komachi Onozuka <komachi.onozuka@yandex.ru>
2012-10-21 20:46:57 +0400

    Some fixies

--- indiesocial.js
+++ indiesocial.js
@@ -4,7 +4,7 @@ License: New BSD License, http://opensource.org/licenses/BSD-3-Clause
 Repo: https://github.com/komachi/indiesocial */
 var indiesocialServices = {
     'facebook': {
-        'shareURL': 'https://www.facebook.com/sharer.php?u=',
+        'shareURL': 'https://facebook.com/sharer.php?u=',
         'title' : '&t=',
         'fontello': 'icon-facebook',
         'name' : 'Facebook',
@@ -27,7 +27,7 @@ var indiesocialServices = {
         'name' : 'Google+',
     },
     'odnoklassniki' : {
-        'shareURL': 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st._surl=',
+        'shareURL': 'http://odnoklassniki.ru/dk?st.cmd=addShare&st._surl=',
         'title' : '&title=',
         'fontello': 'icon-odnoklassniki',
         'name' : 'Одноклассники',
@@ -45,56 +45,52 @@ var indiesocialServices = {
         'name' : 'FriendFeed',
     },
     'linkedin' : {
-        'shareURL': 'http://www.linkedin.com/shareArticle?mini=true&url=',
+        'shareURL': 'http://linkedin.com/shareArticle?mini=true&url=',
         'title' : '&title=',
         'fontello': 'icon-linkedin',
         'name' : 'LinkedIn',
     },
     'tumblr' : {
-        'shareURL': 'http://www.tumblr.com/share/link?url=',
+        'shareURL': 'http://tumblr.com/share/link?url=',
         'title' : '&name=',
         'fontello': 'icon-tumblr',
         'name' : 'Tumblr',
     },
     'blogger' : {
-        'shareURL': 'http://www.blogger.com/blog_this.pyra?u=',
+        'shareURL': 'http://blogger.com/blog_this.pyra?u=',
         'title' : '&n=',
         'fontello': 'icon-blogger',
         'name' : 'Blogger',
     },
 };
-function indiesocialMakeLink(service) {
-    var init = document.getElementById("indiesocial-init");
-    var aElement = document.createElement("a");
-    init.appendChild(aElement);
-    var titlelink = "";
-    if (init.getAttribute("data-URL")) {
-        var URL = indiesocialServices[service]['shareURL'] + init.getAttribute("data-URL");
-    }
-    else {
-        var URL = indiesocialServices[service]['shareURL'] + window.location;
-    }
-    if (indiesocialServices[service]['title'] !== undefined && init.getAttribute("data-title")) {
-         var titlelink = indiesocialServices[service]['title'] + init.getAttribute("data-title");
-    }
-    aElement.setAttribute("href", encodeURI(URL + titlelink));
-    aElement.setAttribute("class", "indiesocial-" + service);
-    aElement.setAttribute("title", indiesocialServices[service]['name']);
-    aElement.setAttribute("target", "_blank");
-    var innerValue = "";
-    if (init.getAttribute("data-addFontelloIcon") == "true") {
-        var innerValue = innerValue + '<i class="' + indiesocialServices[service]['fontello'] + '"></i>';
-    }
-    if (init.getAttribute("data-addText") == "true") {
-        var innerValue = innerValue + indiesocialServices[service]['name'];
-    }
-    aElement.innerHTML = innerValue;
-};
-function indieSocialInit() {
-    for (var value in indiesocialServices) {
-        if (document.getElementById("indiesocial-init").getAttribute("data-indieSocialServices").indexOf(value) != '-1') {
-            indiesocialMakeLink(value);
+window.onload = function() {
+        for (var service in indiesocialServices) {
+        if (document.getElementById("indiesocial-init").getAttribute("data-indieSocialServices").indexOf(service) != '-1') {
+            var init = document.getElementById("indiesocial-init");
+            var aElement = document.createElement("a");
+            init.appendChild(aElement);
+            if (init.getAttribute("data-URL")) {
+                var URL = indiesocialServices[service]['shareURL'] + init.getAttribute("data-URL");
+            }
+            else {
+                var URL = indiesocialServices[service]['shareURL'] + window.location;
+            }
+            var titlelink = "";
+            if (indiesocialServices[service]['title'] !== undefined && init.getAttribute("data-title")) {
+                 var titlelink = indiesocialServices[service]['title'] + init.getAttribute("data-title");
+            }
+            aElement.setAttribute("href", encodeURI(URL + titlelink));
+            aElement.setAttribute("class", "indiesocial-" + service);
+            aElement.setAttribute("title", indiesocialServices[service]['name']);
+            aElement.setAttribute("target", "_blank");
+            var innerValue = "";
+            if (init.getAttribute("data-addFontelloIcon") == "true") {
+                var innerValue = innerValue + '<i class="' + indiesocialServices[service]['fontello'] + '"></i>';
+            }
+            if (init.getAttribute("data-addText") == "true") {
+                var innerValue = innerValue + indiesocialServices[service]['name'];
+            }
+            aElement.innerHTML = innerValue;
         }
     }
-};
-window.onload = indieSocialInit();
+}
